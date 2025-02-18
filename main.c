045b396124b856630db17438144770d5ce737849
Thanh21062006 <caotatthanh2006@gmail.com>
2025-02-19 00:04:53 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 #include "function.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	openFile();
-	char username[20], password[20];
-	int choice = 0;
+	openFile("book.bin");
+	openFileMember("member.bin");
+	char username[20], password[20], temp = 'l';
+	int choice = 0, size = 0;
 	FILE *admin = fopen("Admin.txt", "r");
 	if(admin == NULL){
 		printf("Cannot open file!");
@@ -16,21 +18,33 @@ int main() {
 	
 	fgets(username,20,admin);
 	username[strcspn(username, "\n")] = '\0';
-	printf("%s", username);
 	fgets(password,20,admin);
 	password[strcspn(password, "\n")] = '\0';
 	fclose(admin);
 	
 	while(1){
-		char checkUsername[20], checkPassword[20];
+		system("color e");
+		char checkUsername[20] = "", checkPassword[20] = "";
 		printf("--------HELLO WORD--------\n");
 		printf("Welcome to the login page\n");
+		
 		printf("Enter uername: ");
 		fgets(checkUsername, sizeof(checkUsername), stdin);
 		checkUsername[strcspn(checkUsername, "\n")] = '\0';
+		
 		printf("Enter password: ");
-		fgets(checkPassword, sizeof(checkPassword), stdin);
-		checkPassword[strcspn(checkPassword, "\n")] = '\0';
+        while(1) {
+            temp = getch();
+            if(temp == 13) {
+                break;
+            }
+            printf("%c", temp);
+            checkPassword[size] = temp;
+            size++;
+            printf("\b*");
+
+        }
+        checkPassword[size] = '\0';
 		
 		if(strcmp(username,checkUsername) == 0 && strcmp(password,checkPassword) == 0){
 			printf("\nlogin successful\n");
@@ -55,15 +69,13 @@ int main() {
 						break;
 					default:
 						printf("invalid choice!\n");
-				}
-			
+						break;
+				}	
 			}
 			break;
 		}else{
 			printf("login falled!\n");
 		}		
 	}
-	
-
 	return 0;
 }
